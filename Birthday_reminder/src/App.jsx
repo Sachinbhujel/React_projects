428ba39b6117446dab6af4e8bdd307e0dc959455
Sachin Bhujel <90704616+Sachinbhujel@users.noreply.github.com>
2024-07-26 09:12:47 +0000

    Changes

---
+++ Birthday_reminder/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Data from './Data'
+
+function App(data) {
+
+  return (
+    <>
+      <div className="App">
+        <Data />
+      </div>
+    </>
+  )
+}
+
+export default App
