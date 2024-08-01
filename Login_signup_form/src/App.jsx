20eda53083e4690a17fe14fb8b14af51190e2488
Sachin Bhujel <90704616+Sachinbhujel@users.noreply.github.com>
2024-08-01 09:39:38 +0000

    Changes

--- Login_signup_form/src/App.jsx
+++ Login_signup_form/src/App.jsx
@@ -2,12 +2,35 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [isLogin, setIsLogin] = useState(true);
 
   return (
-    <>
-      fdg
-    </>
+    <div className='container'>
+      <div className='form-container'>
+        <div className='form-toggle'>
+          <button className={isLogin ? "active" : ""} onClick={() => setIsLogin(true)}>Login</button>
+          <button className={!isLogin ? "active" : ""} onClick={() => setIsLogin(false)}>SignUp</button>
+        </div>
+        {isLogin ? <>
+          <div className='form'>
+            <h2>Login Form</h2>
+            <input type="email" placeholder='Email'/>
+            <input type="password" placeholder='Password'/>
+            <a href='#'>Forgot Password</a>
+            <button>Login</button>
+            <p>Not a Member? <a href="#" onClick={() => setIsLogin(false)}>Sign Up</a></p>
+          </div>
+        </> : <>
+        <div className='form'>
+        <h2>Sign Up Form</h2>
+            <input type="email" placeholder='Email'/>
+            <input type="password" placeholder='Password'/>
+            <input type="password" placeholder='Confirm Password'/>
+            <button>SignUp</button>
+        </div>
+        </>}
+      </div>
+    </div>
   )
 }
 
