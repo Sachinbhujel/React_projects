6819325fdc2788cf352cf5377fae3031de4f1f4d
Sachin Bhujel <90704616+Sachinbhujel@users.noreply.github.com>
2024-08-14 17:48:10 +0000

    Changes

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+import Rating_star from '../../Components/Rating_star/App';
+
+function App() {  
+
+  return (
+    <>
+      {/* <Rating_star/> */}
+    </>
+  );
+}
+
+export default App;
