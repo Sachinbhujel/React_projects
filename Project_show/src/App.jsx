f4995cc31cad439e40de47f4c7f8d770e257cca3
Sachin Bhujel <90704616+Sachinbhujel@users.noreply.github.com>
2024-08-17 10:57:38 +0000

    Changes

---
+++ Project_show/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css';
+import Image_slider from '../../Components/Image_slider/App';
+import Load_more_button from '../../Components/Load_more_button/App';
+import Tree_view from '../../Components/Tree_view';
+
+function App() {  
+
+  return (
+    <>
+      {/* <Image_slider/> */}
+      {/* <Load_more_button /> */}
+      <Tree_view />
+    </>
+  );
+}
+
+export default App;
--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,17 +1,13 @@
-import './App.css';
-import Image_slider from '../../Components/Image_slider/App';
-import Load_more_button from '../../Components/Load_more_button/App';
-import Tree_view from '../../Components/Tree_view';
+import React from 'react'
+import './App.css'
 
-function App() {  
+function App() {
 
   return (
     <>
-      {/* <Image_slider/> */}
-      {/* <Load_more_button /> */}
-      <Tree_view />
+      
     </>
-  );
+  )
 }
 
-export default App;
+export default App
